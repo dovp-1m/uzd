21e852ffd2cc0bf7d055016c32414dd9964f567c
dovp-1m <dovydas.petrosius@mif.stud.vu.lt>
2025-04-29 16:35:50 +0300

    Comment

---
+++ lamp_install.sh
@@ -0,0 +1,5 @@
+# Your task is to write an installation script for the LAMP stack (Linux, NGINX, MariaDB, PHP), learn to download source code, compile software on your machine and deploy software and related libraries in /opt/.  
+# It is also important to download, compile and install in /opt/ all components of a LAMP stack 
+# (Linux, NGINX, MariaDB, PHP) on your Debian virtual machine without using any package managers for the installations.
+
+
