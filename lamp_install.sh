a9c8538f7224d874d87e5d1003bd80c1a5e315b1
dovp-1m <dovydas.petrosius@mif.stud.vu.lt>
2025-05-06 16:10:59 +0300

    NGINX install script part

--- lamp_install.sh
+++ lamp_install.sh
@@ -1,5 +1,15 @@
-# Your task is to write an installation script for the LAMP stack (Linux, NGINX, MariaDB, PHP), learn to download source code, compile software on your machine and deploy software and related libraries in /opt/.  
-# It is also important to download, compile and install in /opt/ all components of a LAMP stack 
-# (Linux, NGINX, MariaDB, PHP) on your Debian virtual machine without using any package managers for the installations.
+sudo apt-get update
+sudo apt-get install -y wget build-essential libssl-dev libxml2-dev libcurl4-openssl-dev libjpeg-dev libpng-dev libfreetype6-dev libmcrypt-dev libreadline-dev unzip
 
+mkdir -p /opt/src /opt/nginx /opt/mariadb /opt/php
+
+cd /opt/src
+wget https://nginx.org/download/nginx-1.26.3.tar.gz
+tar -zxvf nginx-1.26.3.tar.gz
+cd nginx-1.26.3
+./configure --prefix=/opt/nginx
+make
+sudo make install
+
+/opt/nginx/sbin/nginx
 
